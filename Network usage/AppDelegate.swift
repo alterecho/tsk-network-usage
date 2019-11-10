4f87863095a35d7b20b7c327dff595873ba70bbd
Vijay Chandran <com.vjc@gmail.com>
2019-11-10 22:59:33 +0800

    Initial Commit

---
+++ Network usage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Network usage
+//
+//  Created by v.a.jayachandran on 10/11/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Network usage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Network usage
+//
+//  Created by v.a.jayachandran on 10/11/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
