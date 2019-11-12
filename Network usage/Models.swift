b6f911416bfaab4b7cd3347bbde0a7899c82fd6c
Vijay Chandran <com.vjc@gmail.com>
2019-11-12 15:28:10 +0800

    added bvase URL

--- Network usage/Models.swift
+++ Network usage/Models.swift
@@ -8,4 +8,39 @@
 
 import Foundation
 
+enum Errors: Error {
+    case decodeError(String)
+}
+
+struct UsageResponse: Decodable {
+
+    struct Result: Decodable {
+
+    }
+    
+    let help: URL
+    let success: Bool
+    let result: UsageResponse.Result
+
+    enum CodingKeys: String, CodingKey {
+        case help
+        case success
+        case result
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let helpString = try container.decode(String.self, forKey: .help)
+        if let help = URL(string: helpString) {
+            self.help = help
+        } else {
+            throw Errors.decodeError("Unable to decode 'help'")
+        }
+        success = try container.decode(Bool.self, forKey: .success)
+        result = try container.decode(UsageResponse.Result.self, forKey: .result)
+    }
+
+    
+}
+
 
---
+++ Network usage/URLs.swift
@@ -0,0 +1,15 @@
+//
+//  URLs.swift
+//  Network Usage
+//
+//  Created by v.a.jayachandran on 12/11/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+import Foundation
+
+//https://data.gov.sg/"api/action/datastore_s earch?resource_id=a807b7ab-6cad-4aa6- 87d0-e283a7353a0f&limit=5
+
+class URLs {
+    static let base = URL(string: "https://data.gov.sg/")
+}
