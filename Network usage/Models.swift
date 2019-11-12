b1863db1fbffd0d02087982cb47912da711fd7de
Vijay Chandran <com.vjc@gmail.com>
2019-11-12 15:28:56 +0800

    added response models

--- Network usage/Models.swift
+++ Network usage/Models.swift
@@ -16,6 +16,48 @@ struct UsageResponse: Decodable {
 
     struct Result: Decodable {
 
+        struct Field: Decodable {
+            let type: String
+            let id: String
+        }
+
+        struct Record: Decodable {
+            let type: String
+            let id: String
+        }
+
+        struct PageLinks: Decodable {
+            let start: URL
+            let next: URL
+
+            enum CodingKeys: String, CodingKey {
+                case start, next
+            }
+
+            init(from decoder: Decoder) throws {
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                var urlStr = try container.decode(String.self, forKey: .next)
+                if let url = URL(string: URLs.base + urlStr) {
+                    start = url
+                } else {
+                    throw Errors.decodeError("next decode error")
+                }
+                urlStr = try container.decode(String.self, forKey: .next)
+
+                if let url = URL(string: URLs.base + urlStr) {
+                    next = url
+                } else {
+                    throw Errors.decodeError("start decode error")
+                }
+            }
+        }
+
+        let resourceID: String
+        let fields: [[String : String]]
+        let records: [[String : String]]
+        let links: PageLinks
+        let limit: Int
+        let total: Int
     }
     
     let help: URL
@@ -39,6 +81,8 @@ struct UsageResponse: Decodable {
         success = try container.decode(Bool.self, forKey: .success)
         result = try container.decode(UsageResponse.Result.self, forKey: .result)
     }
+
+
 
     
 }
