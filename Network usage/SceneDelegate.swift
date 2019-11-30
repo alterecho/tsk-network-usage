08f07da8e7d6d3ba2867967dccd764b41de6b17d
Vijay Chandran <com.vjc@gmail.com>
2019-11-30 18:15:40 +0800

    fix for iOS 12.2 test failure

--- Network usage/SceneDelegate.swift
+++ Network usage/SceneDelegate.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    @available(iOS 13.0, *)
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
     }
-    @available(iOS 13.0, *)
+
     func sceneDidBecomeActive(_ scene: UIScene) {
         TestArgsHandler.handleTestArgs()
     }
